b2e95d85d9eb13b4a59ef9617d5fc5a234badca5
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-08-16 14:40:30 +0530

    Add files via upload

---
+++ bro.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+int main() {
+
+
+    // Q--- Largest Num checker---
+    // int a, b, c;
+    // cout << "Enter a numbers: ";
+    // cin>>a;
+    // cout << "Enter b numbers: ";
+    // cin>>b;
+    // cout << "Enter c numbers: ";
+    // cin>>c; 
+    // if (a > b && a > c) {
+    //     cout << "The largest number is: " << a << endl;
+    // } else if (b > a && b > c) {
+    //     cout << "The largest number is: " << b << endl;
+    // } else {
+    //     cout << "The largest number is: " << c << endl;
+    // }
+
+
+    // Q-- age checker--
+
+    // int age;
+    // cout << "Enter your age: ";
+    // cin >> age;
+    // if (age < 18) {
+    //     cout << "You are a minor." << endl;
+    // } else if (age >= 18 && age < 65) {
+    //     cout << "You are an adult." << endl;
+    // } else {
+    //     cout << "You are a senior citizen." << endl;
+    // }
+
+    // Q1.----Sum of 1 to n Number----
+
+    // int n, sum = 0;
+    // cout << "Enter a number of n : "<<endl;
+    // cin >> n;
+    // // cout << n*(n + 1) / 2 << endl;
+    // for (int i = 1; i<=n; i++)
+    // {
+    //     sum=sum+i;
+    // }    
+    //      cout<<"Total sum ="<<sum<<endl;
+    return 0;
+}
