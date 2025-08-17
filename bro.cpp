f1f7ed5bd124137a3ae5c0021684aab0820cc9f1
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-08-17 16:18:00 +0530

    Add factorial calculation to bro.cpp
    Implemented code to calculate and print the factorial of a user-input number in bro.cpp. Also added .vscode/settings.json for C++ file association.

--- bro.cpp
+++ bro.cpp
@@ -103,17 +103,121 @@ int main() {
     // }
     // cout<<"Total sum of odd numbers = "<< oddsum<<endl;
 
-    // I Love you Diamond useing Do while loop 
-    int n;
-    cout << "Enter a number of n : "<<endl;
-    cin >> n;
-    do
-    {
-        cout<<n<<"  "<<"I Love You Diamond"<<endl;
-        n--;
+// // I Love you Diamond useing Do while loop 
+    // int n;
+    // cout << "Enter a number of n : "<<endl;
+    // cin >> n;
+    // do
+    // {
+    //     cout<<n<<"  "<<"I Love You Diamond"<<endl;
+    //     n--;
 
-    } while (n > 0);
+    // } while (n > 0);
+    
+// Q-- Check if a number is prime or not?
+    // int n;
+    // cout << "Enter a number to check if it is prime: ";
+    // cin >> n;
+    // for (int i=2;i<=n-1;i++) {
+    //     if (n % i == 0) {
+    //         cout << n << " is not a prime number." << endl;
+    //         break;
+    //     }else  {
+    //         cout << n << " is a prime number." << endl;
+    //         break;
+    //     }
+    // }
+//////------------Type 2 Prime Number Check----------------
+
+    // int n;
+    // bool isPrime = true;
+    // cout << "Enter a number to check if it is prime: "; 
+    //  cin >> n;
+    //  for(int i = 2; i <= n - 1; i++) {
+    //      if (n % i == 0) {
+    //          isPrime = false;
+    //          break;
+         
+    //    }
+    //          }
+    //      if(isPrime==true) {
+    //          cout << n << " is a prime number." << endl;
+    //      } else {
+    //          cout << n << " is not a prime number." << endl;
+    //      }
+     // ////////////------------Type 3 Prime Number Check----------------
+    // int n, i = 2;
+    // cout << "Enter a number to check if it is prime: ";
+    // cin >> n;
+    // if (n <= 1) {
+    //     cout << n << " is not a prime number." << endl;
+    //     return 0;}
+    // while(i*i<=n) {
+    //     if (n % i == 0) {
+    //         cout << n << " is not a prime number." << endl;
+    //         break;
+    //     } 
+     
+    //     i++;
+    //     cout << n << " is a prime number." << endl;
+    // }
+
     
 
+// cout << "Enter a number to check if it is prime: ";
+// cin >> n;
+// while(i <=n-1) {
+//     if(n%i==0 && i!=n) {
+//         cout << n << " is not a prime number." << endl;
+//      break;
+//     }
+//     else 
+//     {
+//         i++;
+//     }
+//         }
+//         cout << n << " is a prime number." << endl;
+
+// Q----Nested Loop Example: Multiplication Table
+    // int n,m;
+    // cout << "Enter a number of n : "<<endl;   
+    // cin >> n;
+    // // cout << "Enter a number of m : "<<endl;
+    // // cin >> m;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //        cout<<"Radhe-Radhe ";  
+    //     }
+    //     cout <<"Radhe-Radhe " << endl;
+    // }
+    // Q----Nested Loop Example: Multiplication Table
+    // int n;
+    // cout << "Enter a number of n : "<<endl;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         cout<<"Ram-ji ";}
+    //         cout << endl;
+    //     }
+// Q--Sum of all number form 1 to n which are divisible by 3.
+    // int n, sum = 0;
+    // cout << "Enter a number of n : "<<endl;
+    // cin>>n;
+    // for (int i = 1; i <= n; i++) {
+    //     if (i % 3 == 0) {
+    //         sum += i;
+    //     }
+    // }
+    // cout << "Total sum of numbers divisible by 3 = " << sum << endl;
+
+    // Q-- print factorial of a number n.
+    int n;
+    long long factorial = 1;
+    cout << "Enter a number to find its factorial: ";
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        factorial *= i;
+    }
+    cout << "Factorial of " << n << " is " << factorial << endl;
     return 0;
 }
