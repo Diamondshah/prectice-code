ed81b4ffa8bc2d1d72563348eabd2bb2b6406540
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-08-17 22:47:03 +0530

    Add various pattern examples to Pattern.cpp
    Commented and implemented multiple pattern examples, including square, star, and character patterns. Also added a number pattern that prints consecutive numbers in a grid. Previous 'hello' output was commented out.

--- Pattern.cpp
+++ Pattern.cpp
@@ -1,6 +1,68 @@
 #include<iostream>
 using namespace std;
 int main() {
-    cout<<"hello"<<endl;
+    // cout<<"hello"<<endl;
+    // int n;
+    // cout << "Enter a number of n : "<<endl;
+    // cin >> n;
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= n; j++) {
+    //         cout<<"Diamond ";
+    //     }
+    //     cout << endl;
+    // }   
+// ##--------------Square Pattern----------
+// -----Easy with numbers--like n=1,2,3...etc. 
+    // int n;
+    // cout<<"Enter The value of n :\n";
+    // cin>>n;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++)//😂--=1 miss kar diaa tha maine 
+    //     {
+    //         cout<<j<<" ";
+    //     }cout<<"\n";
+    // }
+
+// ///-----Easy with star--like n=1,2,3...etc number of star in square patter. 
+    // int n;
+    // cout<<"Enter The value of n :\n";
+    // cin>>n;
+    // for(int i=1;i<=n;i++){
+    //     for(int j=1;j<=n;j++)//😂
+    //     {
+    //         cout<<"*"<<" ";
+    //     }cout<<"\n";
+    // }
+                        // -----Charecter Pattern----
+        //     int n;
+        //     cout<<"Enter the number of charecter : ";
+        //     cin>>n;
+        //     if(0<n<27){
+        //     for(int i=0;i<n;i++){
+        //         char ch='A';
+        //         for(int j=0;j<n;j++){
+        //             cout<<ch<<" ";
+        //             ch=ch+1;
+        //         }
+        //         cout<<"\n";
+        //     }
+        // }else{
+        //     cout<<"Aukat mai rah le ";
+        // }
+///////// Q---- Number continue not repirt::-----
+        int n=3;
+        
+
+       
+        for(int i=1;i<=n;i++){
+          
+            for(int j=1;j<=n ;j++){
+                cout<<n;
+                n=n+1;
+            }
+            cout<<"\n";
+        }
+     
+
     return 0;
 }
