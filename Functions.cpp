ff525454c9c5887a640ca74bb782aa60e70a1ad9
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-08-31 10:04:45 +0530

    Add function to find maximum of three numbers
    Implemented a new function 'max' that returns the maximum of three integers. Updated main to prompt user for three values and display the maximum. Commented out previous function examples for clarity.

--- Functions.cpp
+++ Functions.cpp
@@ -1,12 +1,83 @@
 #include<iostream>
 using namespace std;
-int main(){
-    // cout<<"testing"<<endl;
+// cout<<"testing"<<endl;
+// //////////function Define////////////////
+
     ////////////////FUNCTIONS//////////////////////
     // function is a block of code which performs a specific task
     // function is used to avoid repetition and make code modular
     // function is a reusable block of code
 
-    
+// return_type function_name(arguments){
+//  void printHello(){
+//     cout<<"Hello World"<<endl;
+//  }
+
+/// //////////////function the sum of two numbers////////////////////
+// int sum(int a,int b){
+//     int c=a+b;
+//     return c;
+
+// } 
+// int main(){
+//     // printHello(); // function call
+//     // printHello();
+//     // printHello();
+//     // printHello();
+//     // printHello();
+//     // printHello();
+//         // printHello(); // function call
+
+//         int a,b;
+//         cout<<"Enter the value of a"<<endl;
+//         cin>>a;
+//         cout<<"Enter the value of b"<<endl;
+//         cin>>b;
+//         int ans=sum(a,b); // function call
+//         cout<<"The sum is "<<ans<<endl; // print the sum
+//     return 0;
+// }
+
+
+////////////////////function to find the maximum of two numbers////////////////////
+// int max(int a,int b){
+//     if(a>b){
+//         return a;
+//     }else{
+//         return b;
+//     }
+// }
+
+//     int main(){
+//         int a,b;
+//         cout<<"Enter the value of a"<<endl;
+//         cin>>a;
+//         cout<<"Enter the value of b"<<endl;
+//         cin>>b;
+//         int yelo=max(a,b); // function call
+//         cout<<"The maximum is "<<yelo<<endl; // print the maximum
+//         return 0;
+//     }
+
+////////////////////function to find the maximum of three numbers////////////////////
+int max(int a,int b,int c){
+    if(a >= b && a >= c){
+        return a;
+    } else if(b >= a && b >= c){
+        return b;
+    } else {
+        return c;
+    }
+}
+int main(){
+    int a,b,c;
+    cout<<"Enter the value of a";
+    cin>>a;
+    cout<<"Enter the value of b";
+    cin>>b;
+    cout<<"Enter the Value of c";
+    cin>>c;
+
+    cout<<"Maximume num of three "<<max(a,b,c);
     return 0;
 }
