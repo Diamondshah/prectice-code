3480ad6421525396a6f6219bb3e692a3d219aec8
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-08-31 14:24:37 +0530

    Replace max function with sum of N implementation
    Commented out the previous function to find the maximum of three numbers and its usage in main. Added a new function to calculate the sum of numbers from 1 to N, updated main to demonstrate this functionality.

--- Functions.cpp
+++ Functions.cpp
@@ -60,24 +60,45 @@ using namespace std;
 //     }
 
 ////////////////////function to find the maximum of three numbers////////////////////
-int max(int a,int b,int c){
-    if(a >= b && a >= c){
-        return a;
-    } else if(b >= a && b >= c){
-        return b;
-    } else {
-        return c;
-    }
-}
-int main(){
-    int a,b,c;
-    cout<<"Enter the value of a";
-    cin>>a;
-    cout<<"Enter the value of b";
-    cin>>b;
-    cout<<"Enter the Value of c";
-    cin>>c;
+// int max(int a,int b,int c){
+//     if(a >= b && a >= c){
+//         return a;
+//     } else if(b >= a && b >= c){
+//         return b;
+//     } else {
+//         return c;
+//     }
+// }
+// int main(){
+//     int a,b,c;
+//     cout<<"Enter the value of a";
+//     cin>>a;
+//     cout<<"Enter the value of b";
+//     cin>>b;
+//     cout<<"Enter the Value of c";
+//     cin>>c;
 
-    cout<<"Maximume num of three "<<max(a,b,c);
-    return 0;
-}
+//     cout<<"Maximume num of three "<<max(a,b,c);
+//     return 0;
+// }
+
+/////////Calculate Sum of numbers from 1 to N.///////////
+
+        int sumofN(int n){
+            int sum=0;
+            for(int i=1;i<=n;i++){
+                sum=sum+i;
+            }
+            return sum;
+        }
+        int main(){
+        int n;
+        cout<<"Enter the Value of N :";
+        cin>>n;
+        cout<<sumofN(n)<<endl;
+        cout<<sumofN(55)<<endl;
+        cout<<sumofN(10)<<endl;
+        return 0;
+        }
+
+//////////////
