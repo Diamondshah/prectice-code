93041352def536869c89a7c207e3dca7fa746ba1
Diamond Kumar Shah <daymandkumarsah@gmail.com>
2025-09-03 01:01:21 +0530

    Switch from decimal-to-binary to binary-to-decimal conversion
    Commented out the decimal-to-binary conversion logic and implemented binary-to-decimal conversion in BinaryNumsystem.cpp. Updated main function to prompt for a binary number and output its decimal equivalent. Removed .vscode/settings.json.

--- BinaryNumsystem.cpp
+++ BinaryNumsystem.cpp
@@ -7,21 +7,41 @@ using namespace std;
 // }
 
 ///// Convert Decimal to Binary Number System./////
-int decToBinary(int decNum){
-    int ans=0;
-    int power=1;
-    while(decNum>0){
-        int rem=decNum%2;
-        decNum=decNum/2;
-        ans=ans+rem*power;
-        power=power*10;
-    }
-    return ans;}
-int main(){
-    int decNum;
-    cout<<"Enter a Decimal Number :";
-    cin>>decNum ;
-    int binary=decToBinary(decNum);
-    cout<<"the Binary Number is :"<<binary<<endl;
-    return 0;
-}
+// int decToBinary(int decNum){
+//     int ans=0;
+//     int power=1;
+//     while(decNum>0){
+//         int rem=decNum%2;
+//         decNum=decNum/2;
+//         ans=ans+rem*power;
+//         power=power*10;
+//     }
+//     return ans;}
+// int main(){
+//     int decNum;
+//     cout<<"Enter a Decimal Number :";
+//     cin>>decNum ;
+//     int binary=decToBinary(decNum);
+//     cout<<"the Binary Number is :"<<binary<<endl;
+//     return 0;
+// }
+
+///// Convert Binary to Decimal Number System./////
+        int binaryToDec(int binNum){
+            int ans=0;
+            int power=1;
+            while(binNum>0){
+                int rem=binNum%10;
+                binNum=binNum/10;
+                ans=ans+rem*power;
+                power=power*2;
+            }
+            return ans;}
+        int main(){
+            int binNum;
+            cout<<"Enter a Binary Number :";
+            cin>>binNum ;
+            int decimal=binaryToDec(binNum);
+            cout<<"the Decimal Number is :"<<decimal<<endl;
+            return 0;
+        }
